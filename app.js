4e2d4b08236b6938e9de5a1c0345b137e9581aff
abdulrafay-1 <abdulrock413@gmail.com>
2024-09-24 23:09:08 +0500

    displayed user answers in result

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const nextBtn = document.getElementById("next-btn")
 let data;
 let index = 0;
 let result = 0;
+let allUserAns = [];
 
 const fetchQuestions = async () => {
     const response = await fetch('https://the-trivia-api.com/v2/questions');
@@ -39,8 +40,10 @@ function nextQuestion() {
 nextBtn.addEventListener("click", nextQuestion)
 
 function calculateResult() {
-    let checkedInp = [...inputArr].find((item) => item.checked === true)
-    const userAns = checkedInp.labels[0].innerText
+    let checkedInp = [...inputArr].find((item) => item.checked === true);
+    const userAns = checkedInp.labels[0].innerText;
+    allUserAns.push(userAns)
+    console.log(allUserAns)
     if (userAns === data[index].correctAnswer) {
         result = result + 10
     }
@@ -49,8 +52,24 @@ function calculateResult() {
 
 function displayResult() {
     container.innerHTML = `
-        <h1 class='text-center'>Result : ${result}/100</h1>
-    `
+    <div class='text-center position-absolute top-50 start-50 translate-middle'>
+    <h1>Result : ${result}/100</h1>
+    <button class="btn btn-primary" onclick="showAnswers()">View Result</button>
+    </div>
+`;
+}
+
+function showAnswers() {
+    container.innerHTML = `
+    <h1 class='text-center'>Result : ${result}/100</h1>
+    ${data.map((item, index) => `
+        <hr/>
+        <h4>Q:${index + 1} ${item.question.text}</h4>
+        <p>correct answer : <b class="text-success">${item.correctAnswer}</b></p>
+        <p>your answer : <b class="${allUserAns[index] === item.correctAnswer ? 'text-success' : 'text-danger'}">${allUserAns[index]}</b></p>
+    `).join('')}
+`;
+
 }
 
 [...inputArr].map(item => item.onchange = () => {
